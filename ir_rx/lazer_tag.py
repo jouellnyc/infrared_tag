ca7dae3ec7408c35d9c1dd945e5b19d788331eb0
JohnO <jouellnyc@gmail.com>
2023-10-08 21:54:13 -0400

    Re-commmit

--- ir_rx/lazer_tag.py
+++ ir_rx/lazer_tag.py
@@ -1,33 +1,56 @@
 import machine
 from all_off import all_off
+from .oled_setup import oled
+
 
 grn  = machine.Pin(18, machine.Pin.OUT)
 yll  = machine.Pin(19, machine.Pin.OUT)
 red  = machine.Pin(20, machine.Pin.OUT)
 
 hits=0
+pri_scan_code=28
 
+def display_hits():
+    print(f"hits: {hits}")
+    oled.fill(0)
+    oled.text(f"We've been hit!", 0, 0)
+    oled.text(f"Total Hits: {hits}", 0, 20)
+    oled.show()
+    
+def display_restart():
+    oled.fill(0)
+    oled.text('Defending...', 0, 20)
+    oled.show()
+    
+def display_surrender():
+    oled.fill(0)
+    oled.text(f"Surrender!", 0, 20)
+    oled.show()
+                
 def lazer_check(data):
     #The 'OK' Button
-     if data == 64:
+     if data == pri_scan_code:
             global hits
-            print(f"hits: {hits}")
             hits+=1
-            print(f"hits: {hits}")
             if hits == 1:
+                display_hits()
                 grn.on()
                 yll.off()
                 red.off()
             if hits == 2:
+                display_hits()
                 grn.off()
                 yll.on()
                 red.off()
-            if hits == 3:    
+            if hits == 3:
+                display_hits()
                 grn.off()
                 yll.off()
                 red.on()
-            if hits == 4:
+                display_surrender()
+            if hits > 3:
                 hits=0
+                display_restart()
                 all_off()
-                
+                               
                 
